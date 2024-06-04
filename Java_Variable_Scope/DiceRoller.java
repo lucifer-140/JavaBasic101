b2434d107986ecad17ce33c9b8dfe2e08a53bd08
Dave Ongkaputra <davejansen140@gmail.com>
2024-06-04 15:49:28 +0700

    New Update

---
+++ Java_Variable_Scope/DiceRoller.java
@@ -0,0 +1,19 @@
+package Java_Variable_Scope;
+
+import java.util.Random;
+
+public class DiceRoller {
+ 
+    Random random;
+    int number;
+    
+    DiceRoller(){
+        random = new Random();
+        roll();
+    }
+    
+    void roll() {
+        number = random.nextInt(6)+1;
+        System.out.println(number);
+    }
+}
