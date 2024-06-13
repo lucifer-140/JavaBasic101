a72106bbf0e68910633f2f8bb2c496846e32756c
Dave Ongkaputra <davejansen140@gmail.com>
2024-06-13 14:24:11 +0700

    update

---
+++ Java_Inner_Class/Outside.java
@@ -0,0 +1,16 @@
+package Java_Inner_Class;
+
+public class Outside {
+
+	String x = "Hello ";
+	
+	public class Inside{
+		
+		String y = "World!";
+		
+		public void Greeting()
+		{
+			System.out.println(x+y);
+		}
+	}
+}
